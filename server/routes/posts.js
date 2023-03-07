65feb8f4890706e302f2edc5cae8e00c583c03c7
Bealeja <117786111+Bealeja@users.noreply.github.com>
2023-03-07 13:51:12 +1100

    updated posts export

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -7,3 +7,8 @@ const router = express.Router();
 // READ
 router.get("/", verifyToken, getFeedPosts);
 router.get("/:userId/posts", verifyToken, getUserPosts);
+
+// UPDATE
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
